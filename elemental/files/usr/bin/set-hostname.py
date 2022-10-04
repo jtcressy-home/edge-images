18f165247db9deb4750ad201483e88f45f17d866
Joel Cressy <joel@jtcressy.net>
2022-10-04 01:47:15 +0000

    elemental-image

--- elemental/files/usr/bin/set-hostname.py
+++ elemental/files/usr/bin/set-hostname.py
@@ -13,37 +13,32 @@ def slugify(s):
 
 def rpiserial():
     try:
-        f = open('/proc/cpuinfo', 'r')
-        for line in f:
-            if line[0:6]=='Serial':
-                cpuserial = line.split(':')[-1].strip()[-8:-1]
-                break
-        f.close()
+        with open('/proc/cpuinfo', 'r') as f:
+            for line in f:
+                if line[0:6]=='Serial':
+                    return line.split(':')[-1].strip()[-8:-1]
     except:
-        cpuserial = "ERROR000000000"
-    return cpuserial
+        return "ERROR000000000"
 
 def rpimfg():
     return "raspberrypi"
 
 def dmiserial():
-    serial = subprocess.check_output("dmidecode -s system-serial-number", shell=True)
-    return slugify(serial)
+    return slugify(subprocess.check_output("dmidecode -s system-serial-number", shell=True).decode("utf-8"))
 
 def dmimfg():
-    mfg = subprocess.check_output("dmidecode -s system-manufacturer", shell=True)
-    return slugify(mfg)
+    return slugify(subprocess.check_output("dmidecode -s system-manufacturer", shell=True).decode("utf-8"))
 
 if os.path.exists("/sys/firmware/dmi"):
     mfg = dmimfg()
     serial = dmiserial()
-    os.system(f"hostnamectl set-hostname {mfg}-{serial}")
 elif os.path.exists("/sys/firmware/fdt"):
     mfg = rpimfg()
     serial = rpiserial()
-    os.system(f"hostnamectl set-hostname {mfg}-{serial}")
 else:
     mfg = "unknown"
     serial = "0"
-    os.system(f"hostnamectl set-hostname {mfg}-{serial}")
+
+print(f"setting hostname to {mfg}-{serial}")
+os.system(f"hostnamectl set-hostname {mfg}-{serial}")
 exit(0)
