0679315a6448ce98ae6e2c9f49de9671e2dbba52
Joel Cressy <joel@jtcressy.net>
2022-10-06 20:26:18 +0000

    fix script

--- overlay/files-iso/installer.sh
+++ overlay/files-iso/installer.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-/usr/bin/elemental install --eject-cd --reboot -c /config.yaml /dev/sda
+# Find the first disk in the system
+device=$(lsblk --json | jq -r '[.blockdevices[] | select(.type == "disk") | .name][0]')
+
+/usr/bin/elemental install --eject-cd --reboot $device
