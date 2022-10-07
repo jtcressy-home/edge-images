7cd2399ce47d86b3445e04d017e91346f88d56cb
Joel Cressy <joel@jtcressy.net>
2022-10-07 20:19:58 +0000

    fix dashboard and branding

--- overlay/files/usr/bin/tailos-dashboard.sh
+++ overlay/files/usr/bin/tailos-dashboard.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-export TEMPFILE
+export TEMPFILE=$(mktemp)
 
-function maketemp() {
-  TEMPFILE=$(mktemp)
-}
-
-while true; do
-  if [ ! -f $TEMPFILE ]; then
-    maketemp()
-  fi
-  echo "" > $TEMPFILE
-  cat /etc/issue | sed 's@\\r@'"$(uname -r)"'@g' | sed 's@\\m@'"$(uname -m)"'@g' | sed 's@(\\l)@@g' >> $TEMPFILE
-  tailscale status >> $TEMPFILE
-  authurl=$(tailscale status --json | jq -r '.AuthURL')
+while true
+do
+  echo "" > $TEMPFILE;
+  cat /etc/issue | sed 's@\\r@'"$(uname -r)"'@g' | sed 's@\\m@'"$(uname -m)"'@g' | sed 's@\\l@'"$(tty)"'@g' | sed 's@\\n@'"$(hostname)"'@g' >> $TEMPFILE;
+  tailscale status >> $TEMPFILE;
+  authurl=$(tailscale status --json | jq -r '.AuthURL');
   if [ "$authurl" != "" ]; then
-    echo $authurl | qrencode -t ANSIUTF8i >> $TEMPFILE
-  fi
-  date >> $TEMPFILE
+    echo $authurl | qrencode -t ANSIUTF8i >> $TEMPFILE;
+  fi;
+  date >> $TEMPFILE;
   
-  clear
-  cat $TEMPFILE
-  sleep 5
+  clear;
+  cat $TEMPFILE;
+  sleep 5;
 done
