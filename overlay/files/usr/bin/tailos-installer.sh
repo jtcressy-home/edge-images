b354af713e184afa4a0696ea2bd306c206b90004
Joel Cressy <joel@jtcressy.net>
2022-10-07 01:13:28 +0000

    fix installer script

--- overlay/files/usr/bin/tailos-installer.sh
+++ overlay/files/usr/bin/tailos-installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Find the first disk in the system
-device=$(lsblk --json | jq -r '[.blockdevices[] | select(.type == "disk") | .name][0]')
+device="/dev/$(lsblk --json | jq -r '[.blockdevices[] | select(.type == "disk") | .name][0]')"
 
 /usr/bin/elemental install --eject-cd --reboot $device
