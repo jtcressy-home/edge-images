d5e7c7b4edaacd8f16bfad95a2c428a02584cfca
Joel Cressy <joel@jtcressy.net>
2022-10-15 22:47:40 +0000

    refactor helper scripts and make upgrade helper

--- overlay/files/usr/bin/edgenode-installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Find the first disk in the system
-device="/dev/$(lsblk --json | jq -r '[.blockdevices[] | select(.type == "disk") | .name][0]')"
-
-/usr/bin/elemental install $device && eject && reboot
