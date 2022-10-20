2794b8f30bb1ccd2e2d6260e7a258eb12ca0eed1
Joel Cressy <joel@jtcressy.net>
2022-10-20 15:39:51 +0000

    more dns fixes

--- overlay/files/etc/tailscale/up-args.sh
+++ overlay/files/etc/tailscale/up-args.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 echo "--reset"
 echo "--ssh"
-echo "--accept-dns"
+if (netstat -tnap | grep -v ":53")
+then
+  echo "--accept-dns"
+fi
 echo "--accept-routes"
 if [ -d /sys/devices/virtual/net/cni0 ];
 then
   echo "--advertise-routes $(ip route | grep src | grep cni0 | awk '{print $1}'),10.43.0.0/16"
 fi
-if [ -f /boot/firmware/tailscale-authkey ];
+if [ -f /oem/tailscale-authkey ];
 then
-  echo "--auth-key file:/boot/firmware/tailscale-authkey"
+  echo "--auth-key file:/oem/tailscale-authkey"
 else
   echo "--qr"
 fi
