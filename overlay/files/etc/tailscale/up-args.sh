ae926d9ff8c84897b632d2a505136916079a5aea
Joel Cressy <joel@jtcressy.net>
2023-01-12 22:34:52 +0000

    strip coredns, prep for microk8s

--- overlay/files/etc/tailscale/up-args.sh
+++ overlay/files/etc/tailscale/up-args.sh
@@ -1,21 +1,37 @@
 #!/bin/sh
+TAGS=""
+ROUTES=""
 echo "--reset"
 echo "--ssh"
+
 # if !(netstat -tnap | grep ":53" > /dev/null)
 # then
 #   echo "--accept-dns"
 # else
 #   echo "--accept-dns=false"
 # fi
-echo "--accept-dns"
-echo "--accept-routes"
-if [ -d /sys/devices/virtual/net/cni0 ];
-then
-  echo "--advertise-routes $(ip route | grep src | grep cni0 | awk '{print $1}'),10.43.0.0/16"
+echo "--accept-dns=false" # prevent collisions with local coredns
+
+if (netstat -tna | grep ":6443" 2>&1 > /dev/null)
+then # we're a kubernetes server!
+  TAGS="${TAGS},tag:dnslb-kubernetes"
 fi
+
+echo "--accept-routes=true"
+
+if [ -d /sys/devices/virtual/net/cni0 ];
+then # we're a kubernetes node with configured CNI!
+  # advertise the pod network route for the node (usually a dynamically assigned /24)
+  ROUTES="${ROUTES},$(ip route | grep src | grep cni0 | awk '{print $1}')"
+  # also advertise the default service IP range
+  ROUTES="${ROUTES},10.43.0.0/16"
+fi
+
+echo "--advertise-routes='${ROUTES}'"
+echo "--advertise-tags='${TAGS}'"
 if [ -f /oem/tailscale-authkey ];
 then
   echo "--auth-key file:/oem/tailscale-authkey"
 else
-  echo "--qr"
+  echo "--json"
 fi
